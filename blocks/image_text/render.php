eeb73d5673bd441372aef8212595aeb4b6601cb7
Chris Stickland <cstick10@gmail.com>
2023-09-24 19:19:16 +0100

    image and text

--- blocks/image_text/render.php
+++ blocks/image_text/render.php
@@ -3,5 +3,36 @@
 ?>
 
 <div <?php echo get_block_wrapper_attributes(['class' => 'image-text-block']); ?> id="query-<?php the_title(); ?>">
-    image and text
+    <div class="image-text-container <?php the_field('color'); ?> <?php the_field('image_alignment_mobile'); ?> <?php the_field('image_alignment_desktop'); ?>">
+        <div class="text-container <?php the_field('color'); ?>">
+            <div class="color-block"></div>
+            <h2><?php the_field('title'); ?></h2>
+            <p><? the_field('text'); ?></p>
+            <?php $link = get_field('link');
+            if ($link) :
+                $link_url = $link['url'];
+                $link_title = $link['title'];
+                $link_target = $link['target'] ? $link['target'] : '_self';
+            ?>
+
+                <a class="btn <?php if (get_field('color') == 'color-white') {
+                                    echo 'btn-red';
+                                } else {
+                                    echo 'btn-white';
+                                } ?>" href="<?php echo esc_url($link_url); ?>" target="<?php echo esc_attr($link_target); ?>">
+                    <?php echo esc_html($link_title); ?>
+                </a>
+            <?php endif; ?>
+        </div>
+        <div class="image-container">
+            <?php $image = get_field('image');
+            $image_alt = $image['alt'];
+            $image_url = $image['url'];
+
+            if (!empty($image)) : ?>
+                <img src="<?php echo $image_url; ?>" alt="<?php echo $image_alt; ?>" />
+            <?php endif; ?>
+        </div>
+    </div>
+    <div class="text-block-hash"></div>
 </div>
--- functions.php
+++ functions.php
@@ -201,7 +201,7 @@ add_filter('register_taxonomy_args', function ($args, $taxonomy) {
 add_filter('register_post_type_args', function ($args, $post_type) {
 
     // Change this to the post type you are adding support for
-    if ('products' === $post_type) {
+    if ('product' === $post_type) {
         $args['show_in_graphql'] = true;
         $args['graphql_single_name'] = 'product';
         $args['graphql_plural_name'] = 'products'; # Don't set, and it will default to `all${graphql_single_name}`, i.e. `allDocument`.
