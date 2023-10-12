f127b5a7bb6eb7ad113674657a9f7b246e6e826c
Chris Stickland <cstick10@gmail.com>
2023-10-12 19:30:07 +0100

    product guide cta

--- blocks/product_guide_cta/render.php
+++ blocks/product_guide_cta/render.php
@@ -6,20 +6,40 @@
 
 <div <?php echo get_block_wrapper_attributes(['class' => 'product-guide-block']); ?>>
     <div class="product-guide-container">
-        <h2><?php the_field('title'); ?></h2>
-        <div class="product-guide-text"><?php the_field('text'); ?></div>
-        <?php if (have_rows('buttons')) :
-            $count = 0;
-            while (have_rows('buttons')) : the_row();
-                $button = get_sub_field('button_link');
-                if ($button) :
-                    $button_url = $button['url'];
-                    $button_title = $button['title'];
-        ?>
-                    <a class="btn btn-black" href="<?php echo esc_url($button_url); ?>"><?php echo esc_html($button_title); ?></a>
-        <?php endif;
-                $count++;
-            endwhile;
-        endif; ?>
+        <?php $image = get_field('background_image');
+        if ($image) :
+
+            // Image variables.
+            $url = $image['url'];
+            $title = $image['title'];
+            $alt = $image['alt']; ?>
+            <img src="<?php echo $url; ?>" alt="<?php echo $alt; ?>" class="product-guide-background-image" />
+        <?php endif; ?>
+        <div class="product-guide-gradients">
+            <div class="product-guide-gradient-white"></div>
+            <div class="product-guide-gradient-blue"></div>
+        </div>
+        <div class="product-guide-content">
+            <div class="white-dash"></div>
+            <h2><?php the_field('title'); ?></h2>
+            <div class="product-guide-text"><?php the_field('text'); ?></div>
+            <?php if (have_rows('buttons')) : ?>
+                <div class="buttons-container">
+                    <?php $count = 0;
+                    while (have_rows('buttons')) : the_row();
+                        $button = get_sub_field('button_link');
+                        if ($button) :
+                            $button_url = $button['url'];
+                            $button_title = $button['title'];
+                    ?>
+                            <a class="btn btn-black <?php if ($count > 0) {
+                                                        echo 'btn-outline';
+                                                    } ?>" href="<?php echo esc_url($button_url); ?>"><?php echo esc_html($button_title); ?></a>
+                    <?php endif;
+                        $count++;
+                    endwhile; ?>
+                </div>
+            <?php endif; ?>
+        </div>
     </div>
 </div>
