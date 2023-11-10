bb2e2c5b0d3aeff7f0aee3a877dc0d0290b9bcac
Chris Stickland <cstick10@gmail.com>
2023-11-10 17:23:45 +0000

    timeline

--- blocks/timeline/render.php
+++ blocks/timeline/render.php
@@ -1,4 +1,4 @@
-<div <?php echo get_block_wrapper_attributes(['class' => 'timeline-slides-block']); ?>>
+<div <?php echo get_block_wrapper_attributes(['class' => 'timeline-slides-block']); ?> id="timeline">
     <?php if (have_rows('timeline_slides')) : ?>
         <?php $count = 0; ?>
         <ul class="timeline-slides">
@@ -6,14 +6,23 @@
                 <li class="timeline-slide <?php if ($count == 0) {
                                                 echo 'active';
                                             } ?>">
-                    <?php $image = get_sub_field('slide_image');
-                    $image_alt = $image['alt'];
-                    $image_url = $image['url'];
 
-                    if (!empty($image)) : ?>
-                        <img class="image-text-background-image" src="<?php echo $image_url; ?>" alt="<?php echo $image_alt; ?>" />
-                    <?php endif; ?>
-                    <h1><?php the_sub_field('slide_title'); ?></h1>
+                    <div class="slide-content">
+                        <?php $image = get_sub_field('slide_image');
+                        $image_alt = $image['alt'];
+                        $image_url = $image['url'];
+                        if (!empty($image)) : ?>
+                            <img class="image-text-background-image" src="<?php echo $image_url; ?>" alt="<?php echo $image_alt; ?>" />
+                        <?php endif; ?>
+                        <div class="slide-text-container">
+                            <div class="color-block"></div>
+
+                            <h3 class="slide-title"><?php the_sub_field('slide_title'); ?></h3>
+                            <div class="slide-message"><?php the_sub_field('slide_text'); ?></div>
+                        </div>
+                        <div class="slide-year"><?php the_sub_field('slide_year'); ?></div>
+
+                    </div>
                 </li>
                 <?php $count++; ?>
             <?php endwhile; ?>
---
+++ blocks/timeline/timeline.js
@@ -0,0 +1,18 @@
+window.addEventListener('DOMContentLoaded', () => {
+  const timeline = document.getElementById('timeline')
+
+  const slideContainer = timeline.querySelector('.timeline-slides')
+  const slides = timeline.querySelectorAll('.timeline-slide')
+
+  slideContainer.addEventListener('scroll', () => {
+    const scrollPosition = slideContainer.scrollLeft
+    // console.log(scrollPosition)
+    slides.forEach((slide) => {
+      slide.classList.remove('active')
+      console.log(slide.offsetLeft)
+      if (slide.offsetLeft == scrollPosition) {
+        slide.classList.add('active')
+      }
+    })
+  })
+})
