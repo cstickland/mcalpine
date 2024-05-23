ebdede0edbb9c38ee7c3c73525d3d4bad4994393
Chris Stickland <cstick10@gmail.com>
2024-05-23 17:20:32 +0100

    details accordion£

--- blocks/product_details/render.php
+++ blocks/product_details/render.php
@@ -1,4 +1,4 @@
-<div <?php echo get_block_wrapper_attributes(['class' => 'product-details']) ?> id='product-details'>
+<div <?php echo get_block_wrapper_attributes(['class' => 'product-details desktop']) ?> id='product-details'>
 
 
     <?php $allowed_blocks = array(
@@ -6,5 +6,25 @@
         'mcalpine/image-text-details'
     ); ?>
     <InnerBlocks allowedBlocks="<?php echo esc_attr(wp_json_encode($allowed_blocks)); ?>" />
+
+</div>
+<div <?php echo get_block_wrapper_attributes(['class' => 'product-details accordion']) ?> id='product-details'>
+
+    <div class="accordion-question-container">
+        <h2>Details</h2>
+
+        <div class="accordion-toggle-icon">
+            <div class="vertical-line"></div>
+            <div class="horizontal-line"></div>
+        </div>
+
+    </div>
+    <?php $allowed_blocks = array(
+        'mcalpine/image-text-product',
+        'mcalpine/image-text-details'
+    ); ?>
+    <div class="accordion-answer">
+        <InnerBlocks allowedBlocks="<?php echo esc_attr(wp_json_encode($allowed_blocks)); ?>" />
+    </div>
 
 </div>
