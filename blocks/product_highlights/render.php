7147e3991625bdd735f657ff5021afd7bbe5524f
Chris Stickland <cstick10@gmail.com>
2024-05-27 15:56:41 +0100

    accordion product

--- blocks/product_highlights/render.php
+++ blocks/product_highlights/render.php
@@ -25,3 +25,38 @@
         </ul>
     <?php endif; ?>
 </section>
+<section id="product-highlights" <?php echo get_block_wrapper_attributes(['class' => 'product-highlights animate accordion ' . get_field('background_color')]); ?> id="query-<?php the_title(); ?>">
+    <div class="accordion-question-container">
+        <h2 class="product-highlights-title"><?php the_field('title'); ?></h2>
+        <div class="accordion-toggle-icon">
+            <div class="vertical-line"></div>
+            <div class="horizontal-line"></div>
+        </div>
+    </div>
+    <div class="accordion-answer">
+        <?php if (have_rows('highlights')) : ?>
+            <ul class="highlights-icon-grid" id="product-highlights">
+                <?php while (have_rows('highlights')) : the_row();
+                ?>
+                    <li>
+                        <div class="highlight-image">
+                            <?php if (get_sub_field('highlight_image')) { ?>
+                                <?php $image = get_sub_field('highlight_image');
+                                $image_alt = $image['alt'];
+                                $image_url = $image['url'];
+                                ?>
+
+                                <?php if (pathinfo($image_url)['extension'] == 'svg') {
+                                    echo file_get_contents($image_url);
+                                } else { ?>
+                                    <img src="<?php echo esc_url($image_url); ?>" alt="<?php echo esc_attr($image_alt); ?>" class="background-image-schematic" />
+                                <?php } ?>
+                            <?php } ?>
+                        </div>
+                        <h5><?php the_sub_field('highlight_text'); ?></h5>
+                    </li>
+                <?php endwhile; ?>
+            </ul>
+        <?php endif; ?>
+    </div>
+</section>
