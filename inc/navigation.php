b8bb5afcf616dbd7de53a8ce2cbe93112cb73e52
Chris Stickland <cstick10@gmail.com>
2024-07-11 14:40:13 +0100

    gloves fix

--- inc/navigation.php
+++ inc/navigation.php
@@ -1,6 +1,8 @@
 <?php
+// set up menus
 
-function mcalpine_kneepads_register_menus() {
+function mcalpine_kneepads_register_menus()
+{
     register_nav_menus(array(
         'main-menu' => esc_html__('Main Menu', 'McAlpine Kneepads'),
         'social' => esc_html__('Social', 'McAlpine Kneepads')
@@ -8,4 +10,3 @@ function mcalpine_kneepads_register_menus() {
 }
 add_action('init', 'mcalpine_kneepads_register_menus')
 
-?>
